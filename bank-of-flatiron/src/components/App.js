d9b338be2d7847c5700f5ce8f42f23655da89539
mutinduhh <jonathan.mutinda@student.moringaschool.com>
2024-08-23 10:59:43 +0300

    css

--- bank-of-flatiron/src/components/App.js
+++ bank-of-flatiron/src/components/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AccountContainer from "./AccountContainer";
+import "./app.css"
 
 function App() {
   return (
     <div className="ui raised segment">
       <div className="ui segment violet inverted">
-        <h2>The Royal Bank of Flatiron</h2>
+        <h2>Bank of Flatiron</h2>
       </div>
       <AccountContainer />
     </div>
